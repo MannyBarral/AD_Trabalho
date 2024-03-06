83e2ad01c2a38ced500f73256cb039c8ec77bdae
fc56943 <fc56943@alunos.ciencias.ulisboa.pt>
2024-03-06 15:30:35 +0000

    Changes to Kuko_data.py

--- Projecto 1 - Ficheiros Python-20240303/kuko_data.py
+++ Projecto 1 - Ficheiros Python-20240303/kuko_data.py
@@ -1,19 +1,33 @@
 """
 Aplicações Distribuídas - Projeto 1 - kuko_data.py
-Grupo: XX
-Números de aluno: XXXXX XXXXX
+Grupo: 17
+Números de aluno: 56943 56922
 """
 import time
 import copy
 from typing import Dict, List, Tuple
 
+questionIdCounter = 0
+
 class Question:
-    def __init__(self,...):
-        self ...
+    """Question Class
+    id (int) - id 
+    question (String) - Question
+    answers (List of Strings) - Possible Answers 
+    k (int) - right answer 
+    """
+    def __init__(self, question, answers, k):
+        self.id = questionIdCounter + 1
+        self.question = question
+        self.answers = answers
+        self.k = k
        
 
     def __str__(self):
-        ...
+        print("Question id: " + self.id + "\n" +
+              "Question: " + self.question)
+        
+    
 
    ...
 
