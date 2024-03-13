8b7c1d151e0499572e7d129db69b84a42cf4dbd7
João Pedro Leal <65555415+jpleal2002@users.noreply.github.com>
2024-03-13 15:24:40 +0000

    first commit

--- Projecto 1 - Ficheiros Python-20240303/kuko_server.py
+++ Projecto 1 - Ficheiros Python-20240303/kuko_server.py
@@ -1,7 +1,7 @@
 """
 Aplicações Distribuídas - Projeto 1 - kuko_server.py
-Grupo: XX
-Números de aluno: XXXXX XXXXX
+Grupo: 17
+Números de aluno: 56943 56922
 """
 
 import sys
@@ -9,20 +9,37 @@ from net_server import *
 from kuko_data import *
 
 
-
-
-
 ### código do programa principal ###
 def main():
-    ...
-    KukoServer = ...
-    KukoData = ..
-    ...
+    
+    if len(sys.argv) != 3:
+        print("Execute python3 kukoserver.py <host> <port>")
+        return
+    
+    host = sys.argv[1]
+    port = int(sys.argv[2])
 
-        
+    KukoServer = Server(host,port)
+    KukoData = Kuko()
+
+    print("The Kuko server is running...")
+    
+    while True:
+        client_socket, client_address = KukoServer.accept()
+
+        print(f"Connection created with {client_address}")
+
+        #receber dados do cliente
+        data = KukoServer.recv(client_socket)
+
+        #Processar as operações dadas pelo cliente
+        response = KukoData.process_command(data)
+
+        # Envia a resposta de volta para o cliente
+        KukoServer.send(client_socket, response)
+
+        # Fecha a conexão com o cliente
+        KukoServer.close(client_socket)
    
-
-
-# 
 if __name__ == "__main__":
     main()
--- Projecto 1 - Ficheiros Python-20240303/net_client.py
+++ Projecto 1 - Ficheiros Python-20240303/net_client.py
@@ -1,26 +1,41 @@
 """
 Aplicações Distribuídas - Projeto 1 - net_client.py
-Grupo: XX
-Números de aluno: XXXXX XXXXX
+Grupo: 17
+Números de aluno: 56943 56922
 """
 import socket
 from sock_utils import *
 
 class net_client:
     def __init__(self, host, port):
-        self ...
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      
     def connect(self):
-        ...
+        try:
+            self.socket.connect((self.host, self.port))
+            print(f"Connected to server {self.host}:{self.port}")
+
+        except Exception as e:
+            print(f"Error connecting to {self.host}:{self.port}: {e}")
+            exit
 
     def recv(self):
-        ...
+        try:
+            data = self.socket.recv(1024).decode()
+            return data
+        except Exception as e:
+            print(f"Error receiving data: {e}")
     
     def send(self, data):
-        ...
+        try:
+            self.socket.sendall(data.encode())
+        except Exception as e:
+            print(f"Error sending data: {e}")
 
     def close(self):
-        ...
+        self.socket.close()
             
     def __del__(self): 
-        ...
+        self.socket.close()
