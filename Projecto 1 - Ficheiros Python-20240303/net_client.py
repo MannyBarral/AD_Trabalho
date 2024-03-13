cf84892dd53b26e467c688bcccca24638f5f0902
fc56943 <fc56943@alunos.ciencias.ulisboa.pt>
2024-03-13 15:19:27 +0000

    Question and QSet working with main Kuko Class

--- Projecto 1 - Ficheiros Python-20240303/net_client.py
+++ Projecto 1 - Ficheiros Python-20240303/net_client.py
@@ -8,16 +8,18 @@ from sock_utils import *
 
 class net_client:
     def __init__(self, host, port):
-        self ...
+        self.host = host 
+        self.port = port 
      
     def connect(self):
-        ...
+        return create_tcp_client_socket(self.host, self.port)
 
     def recv(self):
-        ...
+        return self.connect().recv(1024) #Vai receber 1024bits
     
     def send(self, data):
-        ...
+        conn_sock = self.connect(self.host, self.port)
+        conn_sock.sendall(data)
 
     def close(self):
         ...
