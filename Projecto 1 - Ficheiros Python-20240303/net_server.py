32f387df6a6aef37591267ae2a9acd5a13f79d20
João Pedro Leal <65555415+jpleal2002@users.noreply.github.com>
2024-03-14 17:17:43 +0000

    Realização do net_server.pt

--- Projecto 1 - Ficheiros Python-20240303/net_server.py
+++ Projecto 1 - Ficheiros Python-20240303/net_server.py
@@ -8,25 +8,34 @@ from sock_utils import *
 
 class Server:
     def __init__(self, host, port):
-        self ...
-    
+        self.host = host
+        self.port = port
+        
     def accept(self):
-        ...
+        self.socket = create_tcp_server_socket(self.host, self.port)
 
     def listen(self):
-        ...    
+        try:
+            self.socket.listen(1) 
+        except Exception as e:
+            print(f"Error: {e}")
+            exit
 
     def recv(self):
-        ...
+        try:
+            data = self.socket.recv(1024).decode()
+            return data
+        except Exception as e:
+            print(f"Error receiving data: {e}")
     
     def send(self,texto):
-        ...
+        try:
+            data = self.socket.sendall(texto.encode())
+        except Exception as e:
+            print(f"Error sending data: {e}")
 
     def close(self):
-        ...
+        self.socket.close()
 
     def __del__(self):
-        ...
-
-
-
+        self.socket.close()
